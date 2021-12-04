22f308d8c83c5c53b85600e138696dabd742a8f4
mk7michael <95246830+mk7michael@users.noreply.github.com>
2021-12-04 01:37:10 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,33 @@
+# welcome_assignment_answers
+# Input - All eight questions given in the assignment.
+# Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    # The student doesn't have to follow the skeleton for this assignment.
+    # Another way to implement it is using "case" statements similar to C.
+
+    match question:
+        case "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 \
+              channel posted by a TA?":
+            return " "
+        case "Are encoding and encryption the same? - Yes/No":
+            return "No"
+        case "Is it possible to decrypt a message without a key? - Yes/No":
+            return "No"
+        case "Is a hashed message supposed to be un-hashed? - Yes/No":
+            return "No"
+        case "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - \
+              Use MD5 hash generator and use the answer in your code":
+            return "42b76fe51778764973077a5a94056724"
+        case "Is MD5 a secured hashing algorithm? - Yes/No":
+            return "No"
+        case "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+            return 5
+        case "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+            return 4
+
+
+if __name__ == "__main__":
+    # use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
