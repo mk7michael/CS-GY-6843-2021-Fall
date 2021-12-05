8da27ddde0fa882f09fea1920cce99ed63d55e1e
mk7michael <95246830+mk7michael@users.noreply.github.com>
2021-12-04 22:49:51 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,13 +11,13 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         return "no"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        return "no"
+        return "yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         return "no"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         return "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        return "yes"
+        return "no"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         return 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
