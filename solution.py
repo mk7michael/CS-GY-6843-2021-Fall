fd2ae5d7e9927905307227a35df9d63dc80faeb9
mk7michael <95246830+mk7michael@users.noreply.github.com>
2021-12-04 22:47:30 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -22,9 +22,4 @@ def welcome_assignment_answers(question):
         return 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         return 4
-
-
-if __name__ == "__main__":
-    # use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+    
